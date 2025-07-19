c8d47fd7f9df34a745b3fd46e7d85722e51f73c6
Shantanu <142725957+sv6095@users.noreply.github.com>
2025-07-19 16:46:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ setup(
         "scikit-image",
         "torchvision"
     ],
-    author="Your Name",
+    author="Shantanu V",
     description="A Speech Emotion Recognition system using MFCC and Deep Learning",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="<repo-url>",
+    url="https://github.com/sv6095/Emonity/",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
